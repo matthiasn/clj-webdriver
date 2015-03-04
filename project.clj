70428797a7515ccbba2aa14e02dca4923e8fef9c
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2015-03-05 00:13:56 +0100

    dependencies, Clojure version, .gitignore

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject clj-webdriver "0.6.1"
+(defproject clj-webdriver "0.7.0-SNAPSHOT"
   :description "Clojure API for Selenium-WebDriver"
   :url "https://github.com/semperos/clj-webdriver"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/core.cache "0.5.0"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/core.cache "0.6.4"]
                  [org.clojure/tools.logging "0.2.3"]
                  ;; Exclude these, giving preference to Selenium-WebDriver's
                  ;; dependence on them.
@@ -14,9 +14,9 @@
                                                 org.apache.httpcomponents/httpmime]]
                  [cheshire "2.1.0"]
                  [org.mortbay.jetty/jetty "6.1.25"]
-                 [org.seleniumhq.selenium/selenium-server "2.43.0"]
-                 [org.seleniumhq.selenium/selenium-java "2.43.0"]
-                 [org.seleniumhq.selenium/selenium-remote-driver "2.43.0"]
+                 [org.seleniumhq.selenium/selenium-server "2.44.0"]
+                 [org.seleniumhq.selenium/selenium-java "2.44.0"]
+                 [org.seleniumhq.selenium/selenium-remote-driver "2.44.0"]
                  [com.github.detro/phantomjsdriver "1.2.0"
                   :exclusion [org.seleniumhq.selenium/selenium-java
                               org.seleniumhq.selenium/selenium-server
